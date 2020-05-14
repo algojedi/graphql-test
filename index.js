4f1c953bb86aaaece1abc8bbff3432da4959d81b
algojedi <vibanwait@hotmail.com>
2020-05-14 14:39:20 -0400

    working

--- index.js
+++ index.js
@@ -6,28 +6,29 @@ const { buildSchema } = require("graphql"); //a function that takes js literal t
 
 //to be used in the place where express expects middleware fn
 //funnels request to the correct resolvers
-const graphqlHttp = require("graphql");
+const graphqlHttp = require("express-graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get(
+app.use(
   "/graphql",
   graphqlHttp({
     //define the schema that'll be exposed to the front end
     schema: buildSchema(`
-        Type RootQuery {
+        type RootQuery {
             events: [String!]!
         }
-        Type RootMutation {
+        type RootMutation {
             createEvent(name: String): String 
         }
         schema { 
-            query:  
-            mutation: 
+            query: RootQuery
+            mutation: RootMutation 
 
-        }`),
+        }
+        `),
     //needs to point to valid graphql schema
     //points at js obj that contains resolver fns... resolver names must match
     //essentially, this is the logic behind handling requests
@@ -38,11 +39,16 @@ app.get(
       },
       createEvent: (args) => {
         //as defined above, must return a String. Args is an obj!
-        return args.name;
-      },
-      graphiql: true, //just for dev purposes so we don't need front end to test backend
+        const result = args.name;
+        return result;
+
+      }
     },
+    graphiql: true, //just for dev purposes so we don't need front end to test backend
   })
 );
 
-app.listen(3000);
+const port = 3000;
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
